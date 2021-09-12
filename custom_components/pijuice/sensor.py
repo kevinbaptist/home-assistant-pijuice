0f971ec1c52bb52e840242267d5c9be8abb9edf4
Racailloux <racailloux@sheepy.fr>
2021-09-12 14:07:11 +0200

    Changed: Retructuration of models
    Added: Add state_class to sensors
    Changed : Move from Entity to SensorEntity class
    Changed: Move to native values to avoid temperature conversion management.

--- custom_components/pijuice/sensor.py
+++ custom_components/pijuice/sensor.py
@@ -4,7 +4,7 @@ import os
 import voluptuous as vol
 from datetime import timedelta
 
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
@@ -20,7 +20,11 @@ from homeassistant.const import (
     DEVICE_CLASS_BATTERY,
     DEVICE_CLASS_TEMPERATURE,
     DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_CURRENT)
+    DEVICE_CLASS_CURRENT
+)
+from homeassistant.components.sensor import (
+    STATE_CLASS_MEASUREMENT,
+)
 
 from smbus2 import SMBus
 
@@ -49,16 +53,25 @@ SENSOR_IO_VOLTAGE = "io_voltage"
 SENSOR_IO_CURRENT = "io_current"
 
 SENSOR_LIST = {
-    # [name, class, unit, icon, index, size]
-    SENSOR_BATTERY_STATUS:  ['Battery status',        '',                       '',                      "mdi:flash",       0x40, 1],
-    SENSOR_POWER_STATUS:    ['Power input status',    '',                       '',                      "mdi:power-plug",  0x40, 1],
-    SENSOR_POWER_IO_STATUS: ['Power input IO status', '',                       '',                      "mdi:power-plug",  0x40, 1],
-    SENSOR_CHARGE:          ['Charge',                DEVICE_CLASS_BATTERY,     PERCENTAGE,              "mdi:battery",     0x41, 1],
-    SENSOR_TEMP:            ['Temperature',           DEVICE_CLASS_TEMPERATURE, TEMP_CELSIUS,            "mdi:thermometer", 0x47, 2],
-    SENSOR_BATTERY_VOLTAGE: ['Battery voltage',       DEVICE_CLASS_VOLTAGE,     ELECTRIC_POTENTIAL_VOLT, "mdi:flash",       0x49, 2],
-    SENSOR_BATTERY_CURRENT: ['Battery current',       DEVICE_CLASS_CURRENT,     ELECTRIC_CURRENT_AMPERE, "mdi:current-dc",  0x4b, 2],
-    SENSOR_IO_VOLTAGE:      ['IO voltage',            DEVICE_CLASS_VOLTAGE,     ELECTRIC_POTENTIAL_VOLT, "mdi:flash",       0x4d, 2],
-    SENSOR_IO_CURRENT:      ['IO current',            DEVICE_CLASS_CURRENT,     ELECTRIC_CURRENT_AMPERE, "mdi:current-dc",  0x4f, 2],
+    # [name, device class, state class, unit, icon, index, size]
+    SENSOR_BATTERY_STATUS:
+        ['Battery status',        '',                       '',                      '',                      "mdi:flash",       0x40, 1],
+    SENSOR_POWER_STATUS:
+        ['Power input status',    '',                       '',                      '',                      "mdi:power-plug",  0x40, 1],
+    SENSOR_POWER_IO_STATUS:
+        ['Power input IO status', '',                       '',                      '',                      "mdi:power-plug",  0x40, 1],
+    SENSOR_CHARGE:
+        ['Charge',                DEVICE_CLASS_BATTERY,     STATE_CLASS_MEASUREMENT, PERCENTAGE,              "mdi:battery",     0x41, 1],
+    SENSOR_TEMP:
+        ['Temperature',           DEVICE_CLASS_TEMPERATURE, STATE_CLASS_MEASUREMENT, TEMP_CELSIUS,            "mdi:thermometer", 0x47, 2],
+    SENSOR_BATTERY_VOLTAGE:
+        ['Battery voltage',       DEVICE_CLASS_VOLTAGE,     STATE_CLASS_MEASUREMENT, ELECTRIC_POTENTIAL_VOLT, "mdi:flash",       0x49, 2],
+    SENSOR_BATTERY_CURRENT:
+        ['Battery current',       DEVICE_CLASS_CURRENT,     STATE_CLASS_MEASUREMENT, ELECTRIC_CURRENT_AMPERE, "mdi:current-dc",  0x4b, 2],
+    SENSOR_IO_VOLTAGE:
+        ['IO voltage',            DEVICE_CLASS_VOLTAGE,     STATE_CLASS_MEASUREMENT, ELECTRIC_POTENTIAL_VOLT, "mdi:flash",       0x4d, 2],
+    SENSOR_IO_CURRENT:
+        ['IO current',            DEVICE_CLASS_CURRENT,     STATE_CLASS_MEASUREMENT, ELECTRIC_CURRENT_AMPERE, "mdi:current-dc",  0x4f, 2],
 }
 
 BAT_STATUS_NORMAL = 'normal'
@@ -124,83 +137,76 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     _LOGGER.info("PiJuice: Everything is setup.")
 
 
-class PiJuiceSensor(Entity):
+class PiJuiceSensor(SensorEntity):
     """Implementation of PiJuice sensor."""
 
     def __init__(self, hass, config, name, sensor):
         """Initialize the sensor."""
         self._hass = hass
-        self._client_name = name
-        self._name = SENSOR_LIST[sensor][0]
-        self._class = SENSOR_LIST[sensor][1]
-        self._unit_of_measurement = SENSOR_LIST[sensor][2]
-        self._icon = SENSOR_LIST[sensor][3]
-        self._index = SENSOR_LIST[sensor][4]
-        self._size = SENSOR_LIST[sensor][5]
         self._sensor = sensor
-        self._state = None
         self._config = config
+        self._client_name = name
+        self._attr_name = SENSOR_LIST[sensor][0]
+        self._attr_device_class = SENSOR_LIST[sensor][1]
+        self._attr_state_class = SENSOR_LIST[sensor][2]
+        self._attr_native_unit_of_measurement = SENSOR_LIST[sensor][3]
+        self._attr_icon = SENSOR_LIST[sensor][4]
+        self._index = SENSOR_LIST[sensor][5]
+        self._size = SENSOR_LIST[sensor][6]
+        self._attr_native_value  = None
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"{self._client_name} {self._name}"
+        return f"{self._client_name} {self._attr_name}"
 
     @property
-    def state(self):
+    def native_value(self):
         """Return the state of the device."""
-        return self._state
+        return self._attr_native_value 
 
     @property
-    def unit_of_measurement(self):
+    def device_class(self):
+        """Return the device class of the entity."""
+        if self._attr_device_class != "":
+            return self._attr_device_class
+        else:
+            return "pijuice__input"
+
+    @property
+    def native_unit_of_measurement(self):
         """Return the unit of measurement of the sensor."""
-        return self._unit_of_measurement
+        return self._attr_native_unit_of_measurement
 
     @property
     def icon(self):
         """Return the icon of the sensor."""
         if self._sensor == SENSOR_BATTERY_STATUS:
-            if self._state == BAT_STATUS_NORMAL:
+            if self._attr_native_value == BAT_STATUS_NORMAL:
                 return "mdi:battery"
-            elif self._state == BAT_STATUS_CHARGING_FROM_IN or self._state == BAT_STATUS_CHARGING_FROM_5V_IO:
+            elif self._attr_native_value == BAT_STATUS_CHARGING_FROM_IN or self._attr_native_value == BAT_STATUS_CHARGING_FROM_5V_IO:
                 return "mdi:battery-charging"
-            elif self._state == BAT_STATUS_NOT_PRESENT:
+            elif self._attr_native_value == BAT_STATUS_NOT_PRESENT:
                 return "mdi:battery-off"
         elif self._sensor == SENSOR_POWER_STATUS or self._sensor == SENSOR_POWER_IO_STATUS:
-            if self._state == POWER_INPUT_NOT_PRESENT:
+            if self._attr_native_value == POWER_INPUT_NOT_PRESENT:
                 return "mdi:power-plug-off-outline"
-            elif self._state == POWER_INPUT_BAD or self._state == POWER_INPUT_WEAK:
+            elif self._attr_native_value == POWER_INPUT_BAD or self._attr_native_value == POWER_INPUT_WEAK:
                 return "mdi:power-plug-outline"
-            elif self._state == POWER_INPUT_PRESENT:
+            elif self._attr_native_value == POWER_INPUT_PRESENT:
                 return "mdi:power-plug"
         elif self._sensor == SENSOR_CHARGE:
-            if self._state > 90:
+            if self._attr_native_value > 90:
                 return "mdi:battery"
-            elif self._state > 70:
+            elif self._attr_native_value > 70:
                 return "mdi:battery-80"
-            elif self._state > 50:
+            elif self._attr_native_value > 50:
                 return "mdi:battery-60"
-            elif self._state > 30:
+            elif self._attr_native_value > 30:
                 return "mdi:battery-40"
             else:
                 return "mdi:battery-20"
-        return self._icon
-
-    @property
-    def device_class(self):
-        """Return the device class of the entity."""
-        if self._class != "":
-            return self._class
-        else:
-            return "pijuice__input"
-
-    @property
-    def state_class(self):
-        """Return the state class of the entity."""
-        if self._class != "":
-            return "measurement"
-        else:
-            return ""
+        return self._attr_icon
 
     async def async_update(self):
         """Set up the sensor."""
@@ -214,34 +220,32 @@ class PiJuiceSensor(Entity):
             
             # Scale red values
             if self._sensor == SENSOR_BATTERY_STATUS:
-                self._state = BATTERY_STATUS_LIST[(data[0] >> 2) & 0x03]
+                self._attr_native_value = BATTERY_STATUS_LIST[(data[0] >> 2) & 0x03]
             elif self._sensor == SENSOR_POWER_STATUS:
-                self._state = POWER_INPUT_LIST[(data[0] >> 4) & 0x03]
+                self._attr_native_value = POWER_INPUT_LIST[(data[0] >> 4) & 0x03]
             elif self._sensor == SENSOR_POWER_IO_STATUS:
-                self._state = POWER_INPUT_LIST[(data[0] >> 6) & 0x03]
+                self._attr_native_value = POWER_INPUT_LIST[(data[0] >> 6) & 0x03]
             elif self._sensor == SENSOR_CHARGE:
-                self._state = data[0]
+                self._attr_native_value = data[0]
             elif self._sensor == SENSOR_TEMP:
                 value = data[0]                     #get unsigned value
                 if (data[0] & (1 << 7)):            #unsigned to signed
                     value = value - (1 << 8)
-                if self._unit_of_measurement == TEMP_FAHRENHEIT:
-                    value = round(celsius_to_fahrenheit(value), 2)
-                self._state = value
+                self._attr_native_value = value
             elif self._sensor == SENSOR_BATTERY_VOLTAGE or self._sensor == SENSOR_IO_VOLTAGE:
-                self._state = ((data[1] << 8) | data[0]) / 1000.0
+                self._attr_native_value = ((data[1] << 8) | data[0]) / 1000.0
             elif self._sensor == SENSOR_BATTERY_CURRENT or self._sensor == SENSOR_IO_CURRENT:
                 value = (data[1] << 8) | data[0]    #assembly unsigned value
                 if (value & (1 << 15)):             #unsigned to signed
                     value = value - (1 << 16)
-                self._state = value /1000.0
+                self._attr_native_value = value /1000.0
             else: #should never occurs
-                self._state = data[0]
+                self._attr_native_value = data[0]
             
-            _LOGGER.debug(f"PiJuice: Updated sensor '{self._name}' - new value '{self._state}'")
+            _LOGGER.debug(f"PiJuice: Updated sensor '{self._attr_name}' - new value '{self._attr_native_value}'")
         # Error while reading on I2C bus
         except Exception as error:
-            _LOGGER.warn(f"PiJuice: Error while retrieving data for sensor '{self._name}' (Error code: '{error}')")
+            _LOGGER.warn(f"PiJuice: Error while retrieving data for sensor '{self._attr_name}' (Error code: '{error}')")
             pass
 
 
